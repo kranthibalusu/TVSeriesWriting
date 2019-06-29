124958cb3a7c329cfcbec3be1040224a4c26ef16
kranthi balusu <kranthibalusu@gmail.com>
2019-06-29 11:06:07 -0500

    Add files via upload

---
+++ office/allCharWords.py
@@ -0,0 +1,43 @@
+#reading the csv file 
+import csv
+import operator
+
+#read and sparse data
+f = open("the-office-lines.csv",'r')
+csvreader = csv.reader(f)
+allData = list(csvreader)
+#delete first row, header
+del allData[0]
+
+#total no of lines
+lineNo = allData.__len__()
+print(lineNo)
+
+#find all charecters
+charDict={}
+
+for row in allData:
+    charName = row[5]#charecter name 
+    if charName in charDict:
+        charDict[charName] = charDict[charName]+1
+    else:
+        charDict[charName] = 0
+
+#print all the dictionary data 
+#print(charDict)
+
+
+# order by number or lines 
+sorted_charDict= [(k, charDict[k]) for k in sorted(charDict, key=charDict.get, reverse = True)]
+
+#print(sorted_charDict)
+
+#looking at only the top 15 speaking charecters 
+charDictTop = sorted_charDict[:15]
+#print(charDictTop)
+
+
+with open('topLines.csv', 'w+') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(charDictTop)
+
---
+++ office/kellyWords.py
@@ -0,0 +1,47 @@
+#reading the csv file 
+import csv
+import re
+
+#read and sparse data
+f = open("the-office-lines.csv",'r')
+csvreader = csv.reader(f)
+allData = list(csvreader)
+#delete first row, header
+del allData[0]
+f.close()
+
+#total no of lines
+lineNo = allData.__len__()
+print(lineNo)
+
+# put all the words spoken m y micheal and their count in a dictionary 
+wordsDict={}
+
+for row in allData:
+    charName = row[5]#charecter name
+    if 'kelly' in charName.lower():
+        line=row[4] #line spoken
+        #print(line)
+        for word in line.split():
+            wordFilt = word.lower()
+            wordFilt = re.sub("[^a-zA-Z]+", "", wordFilt)
+            if wordFilt in wordsDict:
+                wordsDict[wordFilt] = wordsDict[wordFilt]+1
+            else:
+                wordsDict[wordFilt] = 1
+
+
+# order by most popular word
+sorted_wordsDict= [(k, wordsDict[k]) for k in sorted(wordsDict, key=wordsDict.get, reverse = True)]
+
+
+#looking at only the top 15 words by micheal 
+wordsDictTop = sorted_wordsDict[:15]
+
+with open('kellyWords.csv', 'w') as myfile:
+     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+     wr.writerows(sorted_wordsDict)
+myfile.close()
+
+
+
